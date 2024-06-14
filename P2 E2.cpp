a2a3aa0c6a70f8b11715f064c4cfebc787bc0b0f
Franklin2023-119030 <franklinmamaniapaza88@gmail.com>
2024-06-13 22:11:00 -0500

    se agrago un ejercicio de estructuras selectivas

---
+++ P2 E2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int compra, paga;
+	cout<<"Ingrese el precio de la compra :";
+	cin>>compra;
+	if (compra>=1000){
+		paga=compra-(compra*0.2);
+		cout<<"El total a pagar sera :"<<paga<<endl;
+	} else{
+		cout<<"El total a pagar sera :"<<compra<<endl;
+	} return 0;
+}
