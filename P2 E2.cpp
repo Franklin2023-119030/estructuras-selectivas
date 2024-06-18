3d44f595422b9df23e630ba8cf37f88db6688137
Franklin2023-119030 <franklinmamaniapaza88@gmail.com>
2024-06-17 21:18:42 -0500

    Se pregunto con cuanto se va a pagar y se dio el vuelto de ser necesario

--- P2 E2.cpp
+++ P2 E2.cpp
@@ -1,16 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int compra, paga;
+	int compra, pago, vuelto;
 	cout<<"Que tal, como esta. "<<endl;
 	cout<<"Ingrese el precio de la compra :";
 	cin>>compra;
+	cout<<"Con cuanto piensa pagar :";
+	cin>>pago;
+	if (pago>=compra){
+
 	if (compra>=1000){
-		paga=compra-(compra*0.2);
-		cout<<"El total a pagar sera :"<<paga<<endl;
+		compra=compra-(compra*0.2);
+		cout<<"El total a pagar sera :"<<compra<<endl;
+		vuelto= pago-compra;
+		cout<<"Su vuelto es :"<<vuelto<<endl;
 	} else{
 		cout<<"El total a pagar sera :"<<compra<<endl;
+		vuelto=pago-compra;
+		cout<<"Su vuelto es :"<<vuelto<<endl;
 	} 
+	} else{
+		cout<<"El dinero es insuficiento . "<<endl;
+	}
 	cout<<"Gracias por su compra."<<endl;
 	return 0;
 }
